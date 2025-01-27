a4b0d6ebe3c7359889fd4ab1bd5e40875243fb8b
reynard93 <reynard.lee@thoughtworks.com>
2025-01-27 16:21:27 +0800

    make docker container work
    remove propshaft

--- config/application.rb
+++ config/application.rb
@@ -18,11 +18,12 @@ module RailsGrainGraphql
     config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    # config.asset_pipeline = :propshaft
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.api_only = true
   end
 end
